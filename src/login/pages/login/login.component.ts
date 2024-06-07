88b1d9f6a39ee94e0a17b76d4620218aace02c6e
kathi <katharina.eiserfey1@gmail.com>
2024-06-07 16:52:34 +0200

    login

--- src/login/pages/login/login.component.ts
+++ src/login/pages/login/login.component.ts
@@ -1,24 +1,35 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {ClassNameService} from "../../../services/class-name.service";
+import {LuxButtonComponent} from "@ihk-gfi/lux-components";
 
 @Component({
   selector: 'kc-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements AfterViewInit {
   @Input() kcContext: any;
+  @ViewChildren(LuxButtonComponent)
+  public buttons: QueryList<LuxButtonComponent>;
 
   constructor(private classNameService: ClassNameService) {
   }
 
-  ngOnInit() {
-  }
 
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.buttons.forEach(button => {
+          button.elementRef.nativeElement.style.width = '100%';
+      });
+
+    });
+  }
   getClassName(classKey: string): string {
     console.log("getClassName", classKey, this.classNameService.getClassName(classKey));
     return this.classNameService.getClassName(classKey);
   }
+
+
 
   onSubmit() {
     console.log("submit")
